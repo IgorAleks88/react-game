f72abe61aa8d16fc0183dc64c88d04fd0cd3eabd
IgorAleks88 <igor.alexeyenko@gmail.com>
2021-02-18 12:18:24 +0200

    initial commit add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist',
+        hot: true,
+        inline: true,
+        open: true,
+        port: 3000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader', 'eslint-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        publicPath: ''
+                    }
+                },
+                { loader: 'css-loader' },
+                { loader: 'sass-loader' }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './index.html'),
+            filename: 'index.html' }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        })
+    ]
+};
